8d10d622e9fe3a5bb8517fa84104b0996a26db2c
Fabrizio <fabrizio.finta@gmail.com>
2017-11-29 17:22:36 +0100

    PrintEven modified

--- week-02/day01/PrintEven/src/PrintEven.java
+++ week-02/day01/PrintEven/src/PrintEven.java
@@ -1,14 +1,13 @@
 public class PrintEven {
     public static void main(String[] args) {
 
-        for (int i = 1; i < 500; i +=2) {
-            System.out.println(i);
-
-        }
-        /*miért nem működik így?
+        /*for (int i = 1; i < 500; i +=2) {
+            System.out.println(i);*/
 
         for (int i = 0; i < 500; i++) {
-            System.out.println(i % 2 = 1);*/
+            if (i % 2 == 1) {
+                System.out.println("Even" + i);
+            }
 
         }
     }
