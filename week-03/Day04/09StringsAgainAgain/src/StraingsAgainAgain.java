b17f5b23962042f51fb514275dfd6f75bfe95c4f
Fabrizio <fabrizio.finta@gmail.com>
2017-12-07 21:16:46 +0100

    feat(StraingAgainAgain)
    StraingAgainAgain(couldnt find the solution stucked with the exercise.)

--- week-03/Day04/09StringsAgainAgain/src/StraingsAgainAgain.java
+++ week-03/Day04/09StringsAgainAgain/src/StraingsAgainAgain.java
@@ -1,5 +1,18 @@
 public class StraingsAgainAgain {
   public static void main(String[] args) {
-    
+    String sample = "I like stars between the words.";
+    stringer(sample);
+  }
+  
+  private static String stringer(String sample) {
+    if (sample.length() == 0){
+      return sample + sample.charAt(0);
+    }
+    else if (sample.charAt(0)== ' '){
+      return " " + stringer(sample.substring(0,sample.length()));
+    }
+    else {
+      return sample.charAt(0) + stringer(sample.substring(0,sample.length()));
+    }
   }
 }
