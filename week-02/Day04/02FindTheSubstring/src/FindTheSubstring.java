983d26392436bf5654512b55c024595f6663a073
Fabrizio <fabrizio.finta@gmail.com>
2017-12-07 23:08:20 +0100

    feat(FindTheSubstring.java)
    FindTheSubstring(Exercise Finished)

--- week-02/Day04/02FindTheSubstring/src/FindTheSubstring.java
+++ week-02/Day04/02FindTheSubstring/src/FindTheSubstring.java
@@ -1,2 +1,28 @@
 public class FindTheSubstring {
+  public static void main(String[] args) {
+    /* Create a function that takes two strings as a parameter
+     * Returns the starting index where the second one is starting in the first one
+     * Returns -1 if the second string is not in the first one
+     *
+     * Example
+     * input: "this is what I'm searching in", "searching"
+     * output: 17 */
+    String wantToSort = "this is what I'm searching in";
+    String sort = "searching";
+    System.out.println(stringSorter(wantToSort, sort));
+    
+    
+  }
+  
+  private static int stringSorter(String wantToSort, String sort) {
+      int charPos = - 1;
+      for (int index = 0; index < wantToSort.length(); index++) {
+        for (int endIndex = index+1; endIndex < wantToSort.length(); endIndex++) {
+          if (wantToSort.substring(index, endIndex).equals(sort)) {
+            charPos = index;
+          }
+        }
+      }
+      return charPos;
+  }
 }
