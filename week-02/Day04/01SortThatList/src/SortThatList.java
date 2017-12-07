588a91f9b5cc73e21fc068d80d5dcfc46614278a
Fabrizio <fabrizio.finta@gmail.com>
2017-12-07 22:32:20 +0100

    feat(SortThatList)
    SortThatList(repeat feature added)

--- week-02/Day04/01SortThatList/src/SortThatList.java
+++ week-02/Day04/01SortThatList/src/SortThatList.java
@@ -1,4 +1,7 @@
+import java.util.*;
+
 public class SortThatList {
+  public static Scanner scanner = new Scanner(System.in);
   public static void main(String[] args) {
     /* Create a function that takes a list of numbers as parameter
      * Returns a list where the elements are sorted in ascending numerical order
@@ -7,11 +10,27 @@ public class SortThatList {
      * Example
      * input [34, 12, 24, 9, 5]
      * output [5, 9, 12, 24, 34] */
-    
-    listSorter();
-    
+    boolean cont = true;
+    while (cont) {
+      System.out.println("Would you like to sort the list as descending order? (true/false)");
+      boolean isDescOrder = scanner.nextBoolean();
+      System.out.println();
+      ArrayList<Integer> unSortedList = new ArrayList<>(Arrays.asList(34, 12, 24, 9, 5));
+      System.out.println("Input: " + unSortedList);
+      System.out.println("Output " + listSorter(unSortedList, isDescOrder));
+      System.out.println();
+      System.out.println("Would You like to repeat? (true/false)");
+      cont = scanner.nextBoolean();
+    }
   }
   
-  private static void listSorter() {
+  private static ArrayList listSorter(ArrayList<Integer> input, Boolean descending) {
+    if (descending){
+      Collections.sort(input, Collections.reverseOrder());
+    }
+    else{
+      Collections.sort(input);
+    }
+    return input;
   }
 }
