5283b8635f3025014fa35f1b4b10f0c94f56d58d
Fabrizio <fabrizio.finta@gmail.com>
2017-12-11 23:17:35 +0100

    new(Student & Teacher & Main.java) feat(GuessingGame.java)
    GuessingGame(Code refactored a bit // cheat function activated, fixes done // game motor modified: better gaming experience, winning, losing platters // )

---
+++ Week-04/Day01/MainTeachersAndStudents/src/MainTeachersAndStudents.java
@@ -0,0 +1,8 @@
+public class MainTeachersAndStudents {
+  public static void main(String[] args) {
+    Student student = new Student();
+    Teacher teacher = new Teacher();
+    
+    student.
+  }
+}
--- week-02/Day05/GuessingGame/src/GuessingGame.java
+++ week-02/Day05/GuessingGame/src/GuessingGame.java
@@ -5,54 +5,75 @@ public class GuessingGame {
   public static Scanner scanner = new Scanner(System.in);
   
   public static void main(String[] args) {
+    
+    //Game loop
     boolean gameRun = true;
     while (gameRun) {
+      
+      //Game options
       System.out.println("Please enter the minimum range of the guessing game: ");
       int minRange = scanner.nextInt();
       System.out.println("Please enter the maximum range of the guessing game: ");
       int maxRange = scanner.nextInt();
       System.out.println();
-      System.out.println("Thank you!");
-      System.out.println();
-  
+      
+      //Lives ranking and cheat codes
       int lives = rangesToLives(maxRange, minRange);
-      //lives = guessingCheat(lives, shallWeStart); //debug needed
+      lives = guessingCheat(lives); //debug needed
   
-        int randomNumber = (int) (Math.random() * maxRange) + minRange;
-        System.out.println(randomNumber);
+      //Number generator
+      int randomNumber = (int) (Math.random() * maxRange) + minRange;
+      System.out.println();
+       // System.out.println(randomNumber); // Just for debugging.
   
-        boolean isThereAnyLivesLeft = true;
-        while (isThereAnyLivesLeft) {
-          isThereAnyLivesLeft = lives > 0;
-          System.out.println("You have " + lives + " lives left.");
-          System.out.println("Which number i thought in the range of " + minRange + " and " + maxRange + "?");
-          int guess = scanner.nextInt();
-          if (! tipChecker(guess, randomNumber)) {
-            lives = lives - 1;
-          } else {
-            System.out.println("Yaaayy you win!");
-            isThereAnyLivesLeft = false;
-          }
+      //Game motor
+      System.out.println("Let's play!");
+      System.out.println();
+      boolean isThereAnyLivesLeft = true;
+      while (isThereAnyLivesLeft) {
+        isThereAnyLivesLeft = lives > 1;
+        System.out.println("You have " + lives + " lives left.");
+        System.out.println("Which number i thought in the range of " + minRange + " and " + maxRange + "?");
+        int guess = scanner.nextInt();
+        if (! tipChecker(guess, randomNumber)) {
+          lives = lives - 1;
+        } else {
+          System.out.println("");
+          System.out.println("***************************");
+          System.out.println("***** CONGRATULATIONS *****");
+          System.out.println("***************************");
+          System.out.println("*** YOU ARE THE WINNER! ***");
+          System.out.println("***************************");
+          isThereAnyLivesLeft = false;
         }
-        if (lives == 0) {
-          System.out.println("The number was: " + randomNumber + ". Better luck next time! :)");
-        }
-  
-        System.out.println();
-        System.out.println("Want to play again? (Yes/No)");
-        String wantToPlay = scanner.nextLine();
-        if (wantToPlay.equalsIgnoreCase("yes")){
-          gameRun = true;
-        } else if (wantToPlay.equalsIgnoreCase("no")){
-          System.out.println("Hope you had a good time! See you!");
-          gameRun = false;
-        }
-        else {
-          System.out.println("You seem to be tired. See you later!");
-          gameRun = false;
-        }
+      } if (lives == 0) {
+        System.out.println("");
+        System.out.println("*****************");
+        System.out.println("*** GAME OVER ***");
+        System.out.println("******************");
+        System.out.println("The number was: " + randomNumber + ". Better luck next time! :)");
       }
+      
+      //Want to continue the game loop user option
+      gameRun = wantToContinue();
     }
+  }
+  
+  private static boolean wantToContinue() {
+    System.out.println();
+    System.out.println("Want to play again? (Yes/No)");
+    String wantToPlay = scanner.nextLine();
+    if (wantToPlay.equalsIgnoreCase("yes")){
+      return true;
+    } else if (wantToPlay.equalsIgnoreCase("no")){
+      System.out.println("Hope you had a good time! See you!");
+      return false;
+    }
+    else {
+      System.out.println("You seem to be tired. See you later!");
+      return false;
+    }
+  }
   
   public static boolean tipChecker(int guess, int number) {
     boolean guessedTheNumber = false;
@@ -83,11 +104,11 @@ public class GuessingGame {
     return lives;
   }
   
-  public static int guessingCheat(int lives, String readyToStart) {
+  public static int guessingCheat(int lives) {
     boolean choice = true;
     while (choice){
       System.out.println("Please type start if you are ready:");
-      String shallWeStart = scanner.nextLine();
+      String readyToStart = scanner.nextLine();
       if (readyToStart.equalsIgnoreCase("start")){
         choice = false;
       } else if (readyToStart.equalsIgnoreCase("cheat1")){
