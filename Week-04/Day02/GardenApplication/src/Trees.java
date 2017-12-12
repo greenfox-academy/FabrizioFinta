7ae0b0d5f111eaf70605fcf4bbeb318677136097
Fabrizio <fabrizio.finta@gmail.com>
2017-12-12 17:19:08 +0100

    feat(Plants.java & Trees.java)
    Plants() main attribution functions created // Trees(status method created) Exercise not finished yet.

--- Week-04/Day02/GardenApplication/src/Trees.java
+++ Week-04/Day02/GardenApplication/src/Trees.java
@@ -1,2 +1,7 @@
-public class Trees {
-}
+public class Trees extends Plants{
+  
+  public void status(){
+    System.out.println((getNeedWater()) ? "The " + getColor() + " color tree needs water" : "The " + getColor() + " color tree doesnt need water.");
+    
+    }
+  }
