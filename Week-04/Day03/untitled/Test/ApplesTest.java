13785c0430ee23a297588447eb5a5ccf012a6b68
Fabrizio <fabrizio.finta@gmail.com>
2017-12-13 14:21:29 +0100

    newdir(Day03) new(Apples.java)
    Apples.java(File created, test directory created, file filled with exercises test runs succesfully)

---
+++ Week-04/Day03/untitled/Test/ApplesTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplesTest {
+  Apples apple = new Apples();
+  @Test
+  void getApple() {
+    assertEquals("apple", apple.getApple());
+  }
+  
+}
