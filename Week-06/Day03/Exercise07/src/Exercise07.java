33b0c8903a629ee9a12659cabe7cde5cd66b1f90
Fabrizio <fabrizio.finta@gmail.com>
2018-01-03 15:47:03 +0100

    feat(Exercise06)
    main method added

--- Week-06/Day03/Exercise07/src/Exercise07.java
+++ Week-06/Day03/Exercise07/src/Exercise07.java
@@ -2,9 +2,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Exercise07 {
-  ArrayList<String> cities = new ArrayList<>(Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA",
-          "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"));
+  public static void main(String[] args) {
+    ArrayList<String> cities = new ArrayList<>(Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA",
+            "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"));
+    cities.stream()
   
-  
-  
+  }
 }
