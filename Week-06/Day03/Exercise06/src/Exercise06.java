3ee5a10404d04407405dd9a573889258a77b90ff
Fabrizio <fabrizio.finta@gmail.com>
2018-01-03 15:39:16 +0100

    new(Exercise06)
    Exercise finished with streams.

---
+++ Week-06/Day03/Exercise06/src/Exercise06.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Exercise06 {
+  public static void main(String[] args) {
+    String myString = "jamaika a jamaikaiake";
+    Map<String, Long> charFreq = myString.chars()
+            .mapToObj(c -> Character.toString((char) c))
+            .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+    System.out.println(charFreq);
+  }
+}
