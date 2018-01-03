6b52dd4360622a0c07ca0c1f269a28e62b67d708
Fabrizio <fabrizio.finta@gmail.com>
2018-01-03 15:44:50 +0100

    new(Exercise07)
    ArrayList pasted in the code, variable name corrected.

---
+++ Week-06/Day03/Exercise07/src/Exercise07.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Exercise07 {
+  ArrayList<String> cities = new ArrayList<>(Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA",
+          "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"));
+  
+  
+  
+}
