106a6052ba7b1d0e8f71e6bdfce63eb86eac1d44
Fabrizio <fabrizio.finta@gmail.com>
2018-01-03 15:52:45 +0100

    feat(Exercise07)
    Exercise Solved with streams

--- Week-06/Day03/Exercise07/src/Exercise07.java
+++ Week-06/Day03/Exercise07/src/Exercise07.java
@@ -1,11 +1,16 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Exercise07 {
   public static void main(String[] args) {
     ArrayList<String> cities = new ArrayList<>(Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA",
             "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"));
-    cities.stream()
-  
+    List<String> filteredCities = cities.stream()
+            .filter(city -> city.startsWith("A") && city.endsWith("I"))
+            .collect(Collectors.toList());
+    System.out.println(filteredCities);
   }
 }
