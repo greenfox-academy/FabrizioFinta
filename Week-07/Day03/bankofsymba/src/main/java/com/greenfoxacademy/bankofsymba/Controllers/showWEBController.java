9e0c7948a48f7bcc9cfd21c5f09fbfd15064a4fd
Fabrizio <fabrizio.finta@gmail.com>
2018-01-10 14:05:46 +0100

    new(Controller & Template & Model)
    Bank account model created, Controller /show: shows the field of the object, it has more dynamic parts as well

---
+++ Week-07/Day03/bankofsymba/src/main/java/com/greenfoxacademy/bankofsymba/Controllers/showWEBController.java
@@ -0,0 +1,28 @@
+package com.greenfoxacademy.bankofsymba.Controllers;
+
+import com.greenfoxacademy.bankofsymba.Models.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class showWEBController {
+  @GetMapping("/show")
+  public String showFields(Model model){
+    BankAccount simba = new BankAccount("Simba", "2000", "lion");
+    model.addAttribute("name", simba.getName());
+    model.addAttribute("article", checkFirstLetter(simba));
+    model.addAttribute("animalType", simba.getAnimalType());
+    model.addAttribute("balance", simba.getBalance());
+    return "show";
+  }
+  
+  private String checkFirstLetter(BankAccount simba) {
+    List<Character> Vowels = new ArrayList<>(Arrays.asList('A', 'E', 'I', 'O', 'U'));
+    return Vowels.contains(simba.getAnimalType().charAt(0)) ? "an" : "a";
+  }
+}
---
+++ Week-07/Day03/bankofsymba/src/main/java/com/greenfoxacademy/bankofsymba/Models/BankAccount.java
@@ -0,0 +1,39 @@
+package com.greenfoxacademy.bankofsymba.Models;
+
+public class BankAccount {
+  
+  private String name;
+  private String balance;
+  private String animalType;
+  
+  public BankAccount(String name, String balance, String animalType) {
+    this.name = name;
+    this.balance = balance;
+    this.animalType = animalType;
+  }
+  
+  public String getName() {
+  
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public String getBalance() {
+    return balance;
+  }
+  
+  public void setBalance(String balance) {
+    this.balance = balance;
+  }
+  
+  public String getAnimalType() {
+    return animalType;
+  }
+  
+  public void setAnimalType(String animalType) {
+    this.animalType = animalType;
+  }
+}
